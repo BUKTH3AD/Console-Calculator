83a3ca4e02b48aca5db70e17d3557646c2ba0d75
Artem Ershov <spider_artem@mail.ru>
2023-02-01 22:48:43 +0300

    New version

--- Console-Calculator/Program.cs
+++ Console-Calculator/Program.cs
@@ -1,79 +1,25 @@
-﻿namespace Console_Calculator
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Console_Calculator
 {
     internal class Program
     {
-        private static bool CheckInput(string input)
+        private static readonly Regex Format = new Regex(@"^(\d+)([+-\/*])(\d+)$");
+        private static int _x;
+        private static int _y;
+
+        private static readonly Dictionary<char, Action<int, int>> Mapping = new Dictionary<char, Action<int, int>>
+    {
+      { '+', Addition },
+      { '-', Subtraction },
+      { '*', Multiply },
+      { '/', Divide }
+    };
+
+        static void Main()
         {
-            if (input == "exit")
-                Environment.Exit(0);
-
-
-            if (input == "help")
-            {
-                Console.WriteLine("Введите два целых положительных числа до 1000, в формате \"x+y\"");
-                return false;
-            }
-
-            if (input.Contains('+') || input.Contains('-') || input.Contains('*') || input.Contains('/'))
-            {
-                return true;
-            }
-            Console.WriteLine("Wrong format");
-            return false;
-        }
-
-        private static bool CheckValueCorrect(string input, out int value)
-        {
-            bool isValid = int.TryParse(input, out value);
-
-            if (!isValid)
-            {
-                Console.WriteLine($"Operand {value} is incorrect");
-                return false;
-            }
-
-            if (value >= 1000 || value < 0)
-            {
-                Console.WriteLine($"Значение {value} не в диапазоне между 1 и 999");
-                return false;
-            }
-
-            return true;
-
-        }
-
-        private static (bool IsParsed, int X, int Y, char symbol) SplitString(string phrase)
-        {
-            char symbol = default;
-            char[] delimiterChars = { '+', '-', '*', '/' };
-
-            foreach (char item in delimiterChars)
-            {
-                if (phrase.Contains(item))
-                    symbol = item;
-            }
-
-
-
-            string[] input = phrase.Split(symbol);
-
-            //if (input[count] == "")
-            //    count++;
-
-            var isXParsed = CheckValueCorrect(input[0], out var x);
-            var isYParsed = CheckValueCorrect(input[1], out var y);
-
-            if (isXParsed && isYParsed)
-            {
-                return (true, x, y, symbol);
-            }
-
-            return (false, 0, 0, symbol);
-        }
-
-        static void Main(string[] args)
-        {
-
             do
             {
                 var input = Console.ReadLine();
@@ -90,31 +36,84 @@
                     continue;
                 }
 
-                switch (result.symbol)
-                {
-                    case '+':
-                        Console.WriteLine(result.X + result.Y);
-                        break;
-                    case '-':
-                        Console.WriteLine(result.X - result.Y);
-                        break;
-                    case '*':
-                        Console.WriteLine(result.X * result.Y);
-                        break;
-                    case '/':
-                        if (result.Y == 0)
-                        {
-                            Console.WriteLine("Делить на ноль нельзя");
-                            break;
-                        }
-
-                        Console.WriteLine((float)result.X / (float)result.Y);
-                        break;
-                    default:
-                        break;
-                }
+                Mapping[result.symbol].Invoke(_x, _y);
 
             } while (true);
+        }
+
+        private static bool CheckInput(string input)
+        {
+            if (input == "exit")
+                Environment.Exit(0);
+
+            if (input == "help")
+            {
+                Console.WriteLine("Введите два целых положительных числа до 1000, в формате \"x{+,-,*,/}y\"");
+
+                return false;
+            }
+
+            if (Format.IsMatch(input))
+                return true;
+
+            Console.WriteLine("Wrong format. Введите два целых положительных числа до 1000, в формате \"x{+,-,*,/}y\"");
+
+            return false;
+        }
+
+        private static bool CheckValueCorrect(string input, out int value)
+        {
+            var isValid = int.TryParse(input, out value);
+
+            if (!isValid)
+            {
+                Console.WriteLine($"Operand {value} is incorrect");
+                return false;
+            }
+
+            if (value < 1000 && value >= 0)
+                return true;
+
+            Console.WriteLine($"Значение {value} не в диапазоне между 1 и 999");
+
+            return false;
+        }
+
+        private static (bool IsParsed, char symbol) SplitString(string phrase)
+        {
+            var match = Format.Match(phrase);
+            var symbol = char.Parse(match.Groups[2].Value);
+            var xString = match.Groups[1].Value;
+            var yString = match.Groups[3].Value;
+
+            var isXParsed = CheckValueCorrect(xString, out _x);
+            var isYParsed = CheckValueCorrect(yString, out _y);
+
+            if (isXParsed && isYParsed)
+            {
+                return (true, symbol);
+            }
+
+            return (false, symbol);
+        }
+
+        private static void Addition(int x, int y) => Console.WriteLine(x + y);
+
+        private static void Subtraction(int x, int y) => Console.WriteLine(x - y);
+
+
+        private static void Multiply(int x, int y) => Console.WriteLine(x * y);
+
+        private static void Divide(int x, int y)
+        {
+            if (_y == 0)
+            {
+                Console.WriteLine("Делить на ноль нельзя");
+
+                return;
+            }
+
+            Console.WriteLine(x / (float)y);
         }
     }
 }
