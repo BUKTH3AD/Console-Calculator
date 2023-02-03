011035fdbc9f2e87c9a096b9674e473d9dea796d
BUKTH3AD <spider_artem@mail.ru>
2023-02-03 11:22:34 +0300

    03.02

---
+++ Console-Calculator/Help.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator
+{
+    internal class Help
+    {
+        public static void General()
+        {
+            Console.WriteLine("Введите два целых положительных числа до 1000, в формате \"x{+,-,*,/}y\"");
+        }
+        
+    }
+}
